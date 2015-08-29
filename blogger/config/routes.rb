602a5f370df14d24cfada61f160378b5397e3d24
Darren Eng <darrenle9@gmail.com>
2015-08-29 15:09:10 -0700

    I0 completed

--- blogger/config/routes.rb
+++ blogger/config/routes.rb
@@ -1,4 +1,7 @@
-Rails.application.routes.draw do
+Blogger::Application.routes.draw do
+  resources :articles
+end
+# Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -53,4 +56,4 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
+#gend
